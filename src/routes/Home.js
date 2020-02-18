4982bb7049caa5acfd177dc9d116be7d4daff3fd
wlehwjdgh <wlehwjdgh@gmail.com>
2020-02-18 17:59:10 +0900

    in office

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,71 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+    {
+        movies(limit: 15, rating: 3) {
+            id
+            medium_cover_image
+        }
+    }
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    return (
+        <Container>
+            <Header>
+                <Title>Apollo 2020</Title>
+                <Subtitle>I love GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {!loading && data.movies && data.movies.map(m => <Movie key={m.id} id={m.id}/>)}
+        </Container>
+    );
+}
